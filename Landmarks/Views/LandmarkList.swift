88849a1a114ee7c3bf5b1f8e343aefa936dd5e08
tichige <s-waymock2@S-wayMock2noMacBook-Pro.local>
2021-02-12 13:57:40 +0900

    isFavoriteのフィルター機能を追加

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoriteOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoriteOnly) || landmark.isFavorite
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                     LandmarkRow(landmark: landmark)
                 }
@@ -22,12 +30,6 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { devicename in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: devicename))
-                .previewDisplayName(devicename)
-        }
-        
+        LandmarkList()
     }
 }
